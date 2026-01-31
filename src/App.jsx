4137329dc6cc5ef3129acdedce2e3841e17f0941
Mugambi <dmaingi90@gmail.com>
2026-01-31 23:03:56 +0300

    fixed two files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,2 @@
-import blogData from "./blog";
-import Header from "./components/Header";
-import About from "./components/About";
-import ArticleList from "./components/ArticleList";
-
-export default function App() {
-  return (
-    <div>
-      <Header name={blogData.name} />
-      <About image={blogData.image} about={blogData.about} />
-      <ArticleList posts={blogData.posts} />
-    </div>
-  );
-}
+import App from "./components/App";
+export default App;
